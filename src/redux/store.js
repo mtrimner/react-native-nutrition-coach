812e2e5b02d574d0647e4017f6927d36458f5bc0
mtrimner <matt.trimner@gmail.com>
2021-07-26 17:38:20 -0500

    setting up basic filestructure

---
+++ src/redux/store.js
@@ -0,0 +1,16 @@
+// import {configureStore} from '@reduxjs/toolkit';
+// import authReducer from './authSlice';
+// import dietReducer from './dietSlice';
+// import createMealReducer from './createMealSlice';
+// import mealsReducer from './mealsSlice';
+
+// const store = configureStore({
+//   reducer: {
+//     auth: authReducer,
+//     diet: dietReducer,
+//     createMeal: createMealReducer,
+//     meals: mealsReducer,
+//   },
+// });
+
+// export default store;
